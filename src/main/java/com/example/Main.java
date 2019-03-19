2cbea06d7fa58b48f0f46104e221afc587b5e28e
yuki.kuda <yuki.kuda@gmail.com>
2019-03-19 11:14:52 +0900

    test

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -58,7 +58,7 @@ public class Main {
     try (Connection connection = dataSource.getConnection()) {
       Statement stmt = connection.createStatement();
       stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+      stmt.executeUpdate("INSERT INTO ticks VALUES (now()) ");
       ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
       ArrayList<String> output = new ArrayList<String>();
@@ -70,6 +70,7 @@ public class Main {
       return "db";
     } catch (Exception e) {
       model.put("message", e.getMessage());
+      
       return "error";
     }
   }
